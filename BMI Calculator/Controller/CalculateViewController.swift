92eb9637e2df77867997d87e7e9dc55f837e4317
manuvela19 <156265199+manuvela19@users.noreply.github.com>
2024-01-24 12:56:15 +0530

    Add files via upload

--- BMI Calculator/Controller/CalculateViewController.swift
+++ BMI Calculator/Controller/CalculateViewController.swift
@@ -9,12 +9,25 @@ import UIKit
 
 class CalculateViewController: UIViewController {
 
+    @IBOutlet weak var heightToChoose: UITextField!
+    
+    @IBOutlet weak var weightToChoose: UITextField!
+    
+    let arr = ["meters", "cm", "feet"]
+    let arr1 = ["kg", "pounds"]
+    
+    var pickerView1 = UIPickerView()
+    var pickerView2 = UIPickerView()
+
+    
     @IBOutlet weak var heightLabel: UILabel!
     
     @IBOutlet weak var weightLabel: UILabel!
     
     @IBOutlet weak var heightSlider: UISlider!
     @IBOutlet weak var weightSlider: UISlider!
+    
+    
     
     var calculatorBrain = CalculatorBrain()
     
@@ -24,15 +37,24 @@ class CalculateViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        pickerView1.delegate = self
+        pickerView1.dataSource = self
+        heightToChoose.inputView = pickerView1
+        
+        pickerView2.delegate = self
+        pickerView2.dataSource = self
+        weightToChoose.inputView = pickerView2
+        
+        
     }
 
     @IBAction func heightSliderChanged(_ sender: UISlider) {
         
-        heightLabel.text = String(format: "%.2f", sender.value) + " meters"
+        heightLabel.text = String(format: "%.2f", sender.value) + " " + heightToChoose.text!
     }
     
     @IBAction func weightSliderChanged(_ sender: UISlider) {
-        weightLabel.text = String(Int(sender.value)) + " kg"
+        weightLabel.text = String(Int(sender.value)) + " " + weightToChoose.text!
     }
     
     
@@ -40,7 +62,9 @@ class CalculateViewController: UIViewController {
         let height = heightSlider.value
         let weight = weightSlider.value
         
-        calculatorBrain.calculateBMI(height : height, weight : weight)
+        let h = heightToChoose.text!
+        let w = weightToChoose.text!
+        calculatorBrain.calculateBMI(height : height, weight : weight, h : h, w : w)
         
         self.performSegue(withIdentifier: "goToResult", sender: self)
     }
@@ -56,5 +80,80 @@ class CalculateViewController: UIViewController {
         }
     }
     
+    @IBAction func unitsButton(_ sender: UIButton) {
+        
+        let h = heightToChoose.text!
+        let w = weightToChoose.text!
+        
+        if(h == "meters"){
+            heightSlider.minimumValue = 0
+            heightSlider.maximumValue = 3
+        }
+        if(h == "cm"){
+            heightSlider.minimumValue = 0
+            heightSlider.maximumValue = 300
+        }
+        if(h == "feet"){
+            heightSlider.minimumValue = 0
+            heightSlider.maximumValue = 10
+        }
+        
+        if(w == "kg"){
+            weightSlider.minimumValue = 0
+            weightSlider.maximumValue = 200
+        }
+        
+        if(w == "pounds"){
+            weightSlider.minimumValue = 0
+            weightSlider.maximumValue = 300
+        }
+        print(h)
+        print(w)
+    }
+    
+    
+}
+
+extension CalculateViewController : UIPickerViewDelegate, UIPickerViewDataSource{
+    public func numberOfComponents(in pickerView1: UIPickerView) -> Int {
+        return 1
+    }
+    
+    
+    
+    public func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        var countrows = arr.count
+        if(pickerView == pickerView2){
+            countrows = self.arr1.count
+        }
+        return countrows
+    }
+    
+    public func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        
+        if(pickerView == pickerView1){
+            let titleRow = arr[row]
+            return titleRow
+        }
+        else if(pickerView == pickerView2){
+            let titleRow = arr1[row]
+            return titleRow
+        }
+        return ""
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+        if(pickerView == pickerView1){
+            self.heightToChoose.text = self.arr[row]
+            heightToChoose.resignFirstResponder()
+        }
+        else if(pickerView == pickerView2){
+            self.weightToChoose.text = self.arr1[row]
+            weightToChoose.resignFirstResponder()
+        }
+        
+
+    }
 }
 
--- BMI Calculator/Models/CalculatorBrain.swift
+++ BMI Calculator/Models/CalculatorBrain.swift
@@ -33,20 +33,98 @@ struct CalculatorBrain{
     
     
     
-    mutating func calculateBMI(height : Float, weight : Float){
+    mutating func calculateBMI(height : Float, weight : Float, h : String, w : String){
+    
         
-         let bmiValue = weight / (height * height)
+        let advice1 = "Eat more pies"
+        let advice2 = "perfect"
+        let advice3 = "EAt less pies"
+        
+        let color1 = UIColor(red: 0, green: 0, blue: 1, alpha: 0.9)
+        let color2 = UIColor(red: 0, green: 1, blue: 0, alpha: 0.9)
+        let color3 = UIColor(red: 1, green: 0, blue: 0, alpha: 0.9)
         
         
-        if(bmiValue < 18.5){
-            bmi = BMI(value: bmiValue, advice: "Eat more pies", color: UIColor(red: 0, green: 0, blue: 1, alpha: 0.9))
+        switch(h, w){
+            
+          case ("meters", "kg") :
+            let bmiValue = weight / (height * height)
+                if(bmiValue < 18.5){
+                    bmi = BMI(value: bmiValue, advice: advice1, color: color1)
+                }
+                else if(bmiValue < 24.9){
+                    bmi = BMI(value: bmiValue, advice: advice2, color: color2)
+                }
+                else{
+                    bmi = BMI(value: bmiValue, advice: advice3, color: color3)
+                }
+            
+          case ("meters", "pounds") :
+            let bmiValue = weight * 0.453592 / (height * height)
+            if(bmiValue < 18.5){
+                bmi = BMI(value: bmiValue, advice: advice1, color: color1)
+            }
+            else if(bmiValue < 24.9){
+                bmi = BMI(value: bmiValue, advice: advice2, color: color2)
+            }
+            else{
+                bmi = BMI(value: bmiValue, advice: advice3, color: color3)
+            }
+            
+        case ("cm", "kg") :
+            let bmiValue = (weight / (height * height)) * 10000
+          if(bmiValue < 18.5){
+              bmi = BMI(value: bmiValue, advice: advice1, color: color1)
+          }
+          else if(bmiValue < 24.9){
+              bmi = BMI(value: bmiValue, advice: advice2, color: color2)
+          }
+          else{
+              bmi = BMI(value: bmiValue, advice: advice3, color: color3)
+          }
+            
+        case ("cm", "pounds") :
+            let bmiValue = (weight * 0.453592 / (height * height)) * 10000
+          if(bmiValue < 18.5){
+              bmi = BMI(value: bmiValue, advice: advice1, color: color1)
+          }
+          else if(bmiValue < 24.9){
+              bmi = BMI(value: bmiValue, advice: advice2, color: color2)
+          }
+          else{
+              bmi = BMI(value: bmiValue, advice: advice3, color: color3)
+          }
+            
+        case ("feet", "kg") :
+            let bmiValue = weight / (height * height * 0.3048 * 0.3048)
+          if(bmiValue < 18.5){
+              bmi = BMI(value: bmiValue, advice: advice1, color: color1)
+          }
+          else if(bmiValue < 24.9){
+              bmi = BMI(value: bmiValue, advice: advice2, color: color2)
+          }
+          else{
+              bmi = BMI(value: bmiValue, advice: advice3, color: color3)
+          }
+            
+        case ("feet", "pounds") :
+            let bmiValue = (weight * 0.453592 / (height * height * 0.3048 * 0.3048)) 
+          if(bmiValue < 18.5){
+              bmi = BMI(value: bmiValue, advice: advice1, color: color1)
+          }
+          else if(bmiValue < 24.9){
+              bmi = BMI(value: bmiValue, advice: advice2, color: color2)
+          }
+          else{
+              bmi = BMI(value: bmiValue, advice: advice3, color: color3)
+          }
+        
+        case (_, _):
+            print("default")
         }
-        else if(bmiValue < 24.9){
-            bmi = BMI(value: bmiValue, advice: "perfect", color: UIColor(red: 0, green: 1, blue: 0, alpha: 0.9))
-        }
-        else{
-            bmi = BMI(value: bmiValue, advice: "EAt less pies", color: UIColor(red: 1, green: 0, blue: 0, alpha: 0.9))
-        }
+        
+        
+        
         
         
         
